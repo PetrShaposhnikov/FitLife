682bd0f9046be896ab8af81a85dd08eb5f2c5bbb
Шапошников Пётр <petrworkit@gmail.com>
2026-08-26 12:36:28 +0300

    correction of the reviewer's comments

--- fit_life.py
+++ fit_life.py
@@ -1,12 +1,10 @@
 # Проект FitLife - MVP версия 1.0
 WATER_PER_KG = 30
-SEPARATE = "*" * 20
+MILILITERS = 1000
 
 # 1. Знакомство
-print(SEPARATE)
 print("Здравсвтуйте, Вас приветствует программа FitLife.")
-print("Давайте рассчитаем Ваш ИМТ и нормы воды в день.")
-print(SEPARATE)
+print("Давайте рассчитаем Ваш ИМТ и нормы воды в день.", sep="\n")
 
 user_name = input("Как Вас зовут? ")
 user_age = int(input("Сколько Вам лет? "))
@@ -18,12 +16,10 @@ user_height = float(input("Какой у Вас рост? "
 
 # 3. Логика расчетов, расчет ИМП и нормы воды
 bmi = round(user_weight / (user_height ** 2), 1)
-water_liters = (user_weight * WATER_PER_KG) / 1000
+water_liters = (user_weight * WATER_PER_KG) / MILILITERS
 
 # 4. Вывод результата
-print(SEPARATE)
 print(f"Отчет пользователя: {user_name} ({user_age} г.)")
 print(f"Ваш индекс массы тела: {bmi}")
 print(f"Рекомендуемая норма воды: {water_liters:.1f} л. в день")
-print("Расчет окончен. Будьте здоровы!")
-print(SEPARATE)
+print("Расчет окончен. Будьте здоровы!", sep="\n")
